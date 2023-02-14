cf8ecb61c1dd044c123ee5a1de21cdf38ea7e071
kobeza1 <102040962+kobeza1@users.noreply.github.com>
2023-02-14 16:39:41 +0200

    bugs

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,7 +5,7 @@ const { joiLoginSchema, joiSignUpSchema } = require("../../models/user");
 
 const router = express.Router();
 
-router.post("/signup", validation(joiSignUpSchema), ctrlWrapper(ctrl.signUp));
+router.post("/signup", validation(joiSignUpSchema), ctrlWrapper(ctrl.signup));
 
 router.post("/login", validation(joiLoginSchema), ctrlWrapper(ctrl.login));
 
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { validation, ctrlWrapper, auth } = require("../../middlewares");
 const { users: ctrl } = require("../../controllers");
+const { subscriptionJoiSchema } = require("../../models/user");
 
 const router = express.Router();
 
@@ -8,6 +9,11 @@ router.get("/", ctrlWrapper(ctrl.getAll));
 
 router.get("/current", auth, ctrlWrapper(ctrl.getCurrent));
 
-router.patch("/", auth, ctrlWrapper(ctrl.changeSubscr));
+router.patch(
+  "/",
+  auth,
+  validation(subscriptionJoiSchema),
+  ctrlWrapper(ctrl.changeSubscr)
+);
 
 module.exports = router;
